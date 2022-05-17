18b6fd1f574453be9b4107e4e93d61ef32732bd2
riricode <105655238+riricode@users.noreply.github.com>
2022-05-17 09:43:32 -0400

    Add files via upload

--- RetirementAccount.java
+++ RetirementAccount.java
@@ -9,28 +9,33 @@ public class RetirementAccount
 	c = new Console ();
 
 	//Variable declaration
-	int accountMoney, withdrawals, moneyLeft = 0, withdrawalsTotal = 0, years = 0;
+	int accountMoney, withdrawals, moneyLeft, withdrawalsTotal, years;
+	
+	//Variable initialization 
+	moneyLeft = 0; 
+	withdrawalsTotal = 0; 
+	years = 0; 
 
 	//Input section
-
 	c.print ("Retirement Account: ");
 	accountMoney = c.readInt ();
 	c.println ();
-	c.println ("Withdrawals"); 
+	c.println ("Withdrawals");
+	c.print("Year 1: "); 
+	withdrawals = c.readInt();
 
-	do 
+	while (withdrawals != -1 || (withdrawalsTotal <= accountMoney))
 	{
 	    years++;
-	    c.print ("Year " + (years) + ": ");
-	    withdrawals = c.readInt ();
 	    withdrawalsTotal = withdrawalsTotal + withdrawals;
-
+	    c.print ("Year " + (years + 1) + ": ");
+	    withdrawals = c.readInt ();
 	}
-	while (withdrawals != -1 || withdrawalsTotal < accountMoney);
 
 	if (withdrawals == -1 || withdrawalsTotal > accountMoney)
 	{ 
 	    moneyLeft = accountMoney - withdrawalsTotal;
+	    c.println();
 	    c.print ("You have $" + moneyLeft + " left");
 	}
 
